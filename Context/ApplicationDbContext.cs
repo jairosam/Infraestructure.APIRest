05ca041dc80d24c6dd220fc2f74395948fd8d4cb
Jairo Buitrago <jairo@granparamo.com>
2023-02-21 22:34:46 -0500

    Context

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -9,13 +9,16 @@ namespace Infraestructure.API.Context
         {
         }
 
-        public DbSet<Colaborador> Colaborador { get; set; }
         public DbSet<Ciudad> Ciudad { get; set; }
+        public DbSet<Colaborador> Colaborador { get; set; }
         public DbSet<Emprendimiento> Emprendimiento { get; set; }
+        public DbSet<Estado> Estado { get; set; }
         public DbSet<Inversor> Inversor { get; set; }
         public DbSet<Ofertas> Ofertas { get; set; }
         public DbSet<Rol> Rol { get; set; }
+        public DbSet<Tarjeta> Tarjeta { get; set; }
         public DbSet<TipoIdentificacion> TipoIdentificacion { get; set; }
+        public DbSet<TipoOferta> TipoOferta { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
     }
 }
